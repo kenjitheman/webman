bb0d5cc2b568ff8812615253a9a9edf0b383a595
kenjitheman <kenjith3man@gmail.com>
2023-11-05 05:13:53 +0200

    feat: main.js (layout)

---
+++ components/layouts/main.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import { AnimatePresence } from "framer-motion";
+import NavBar from "../navbar";
+import { Box, Container } from "@chakra-ui/react";
+import Footer from "../footer";
+
+const Main = ({ children, router }) => {
+    return (
+        <Box as="main" pb={8}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta
+                    name="description"
+                    content="Alt Corporation"
+                />
+                <meta
+                    name="author"
+                    content="Alt Corporation"
+                />
+                <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <link rel="icon" type="image/png" href="/android-chrome-512x512.png" />
+                <link rel="icon" type="image/png" href="/android-chrome-192x192.png" />
+                <link rel="icon" type="image/png" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" href="/favicon-16x16.png" />
+                <meta
+                    property="og:site_name"
+                    content="Alt Corporation"
+                />
+                <meta
+                    name="og:title"
+                    content="Alt Corporation"
+                />
+                <meta property="og:type" content="website" />
+                <title>Alt Corporation</title>
+            </Head>
+
+            <NavBar path={router.asPath} />
+
+            <Container maxW="container.3xl" pt={14}>
+                <AnimatePresence initial={true} mode="wait">
+                    {children}
+                </AnimatePresence>
+                <Footer />
+            </Container>
+        </Box>
+    );
+};
+
+export default Main;
