f70f236efc86c926dba6704d14f89eff93ed848a
kenjitheman <kenjith3man@gmail.com>
2023-11-05 05:07:18 +0200

    feat: font

---
+++ components/fonts.js
@@ -0,0 +1,8 @@
+const Fonts = () => (
+    <style jsx global>
+        {`
+    @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
+  `}
+    </style>
+);
+export default Fonts;
