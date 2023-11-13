86c8182585a3b00a5179d5c1188d214c4e2773cb
kenjitheman <kenjith3man@gmail.com>
2023-11-13 21:36:01 +0200

    chore: modify about page

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Flex } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import SimpleAccordion from '../components/accordion.js'
 
 const About = () => (
     <Layout title="About | Alt Corporation">
-        <Container>
+        <Container maxW="7xl">
             <Heading as="h1" fontSize={35} mt={12} mb={12} textAlign={"center"}>
                 About Us
             </Heading>
@@ -14,14 +15,23 @@ const About = () => (
                     Welcome to our little project!
                 </Paragraph>
             </Section>
-            <Section delay={0.2} mb={12} >
+            <Section delay={0.3} mb={12} >
                 <Paragraph>
                     Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
                 </Paragraph>
                 <br />
-                <Paragraph>
-                    Some more text about us.
-                </Paragraph>
+                <Section delay={0.6}>
+                    <Flex
+                        flexDirection={{ base: 'column', md: 'row' }}
+                    >
+                        <Box w={{ base: '100%', md: '50%' }}>
+                            <SimpleAccordion />
+                        </Box>
+                        <Box w={{ base: '100%', md: '50%' }}>
+                            <SimpleAccordion />
+                        </Box>
+                    </Flex>
+                </Section>
             </Section>
         </Container>
     </Layout>
