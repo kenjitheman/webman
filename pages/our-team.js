ac07ddf29629be24c31a6d31da3bcf7d3cc19f46
kenjitheman <kenjith3man@gmail.com>
2023-11-13 21:36:48 +0200

    chore: modify our team page

--- pages/our-team.js
+++ pages/our-team.js
@@ -4,23 +4,23 @@ import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 
 const OurTeam = () => (
-    <Layout title="Team | Alt Corporation">
+    <Layout title="Our Team | Alt Corporation">
         <Container>
             <Heading as="h1" fontSize={35} mt={12} mb={12} textAlign={"center"}>
-                About Us
+                Our Team
             </Heading>
             <Section delay={0.1} mb={7} >
                 <Paragraph >
-                    Welcome to our little project!
+                    Welcome to our team page!
                 </Paragraph>
             </Section>
             <Section delay={0.2} mb={12} >
                 <Paragraph>
-                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
+                    Thanks for cheking our team page, we are the biggest in Ukraine and we are proud of it! We are Alt Corporation and we are the best! We hope you will enjoy with us!
                 </Paragraph>
                 <br />
                 <Paragraph>
-                    Some more text about us.
+                    We are a small team of developers and designers from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
                 </Paragraph>
             </Section>
         </Container>
