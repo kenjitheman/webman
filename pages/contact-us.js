aa5069bdeec18fa612bd925d763606a6492e7929
kenjitheman <kenjith3man@gmail.com>
2023-11-13 21:29:27 +0200

    feat: contact-us page

---
+++ pages/contact-us.js
@@ -0,0 +1,35 @@
+import { Container, Box, Heading, Flex } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import CallToAction from '../components/call_to_action.js'
+
+const ContactUs = () => (
+    <Layout title="Contact Us | Alt Corporation">
+        <Container maxW="7xl">
+            <Heading as="h1" fontSize={35} mt={12} mb={12} textAlign={"center"}>
+                Contact Us
+            </Heading>
+            <Section delay={0.1} mb={7} >
+                <Paragraph >
+                    Welcome to our little project!
+                </Paragraph>
+            </Section>
+            <Section delay={0.3} mb={12} >
+                <Paragraph>
+                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
+                </Paragraph>
+                <br />
+                <Container maxW="8xl">
+                    <Section delay={0.7}>
+                        <Box>
+                            <CallToAction />
+                        </Box>
+                    </Section>
+                </Container>
+            </Section>
+        </Container>
+    </Layout >
+)
+
+export default ContactUs
