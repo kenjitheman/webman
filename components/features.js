7a51b71f12078abfd51629666adc32a6192ffe41
kenjitheman <kenjith3man@gmail.com>
2023-11-13 21:34:29 +0200

    chore: modify features component

--- components/features.js
+++ components/features.js
@@ -1,6 +1,5 @@
-import { Box, SimpleGrid, Icon, Text, Stack, Flex } from '@chakra-ui/react';
-import { withEmotionCache } from '@emotion/react';
-import { DiVim, DiRust, DiGo } from "react-icons/di";
+import { Box, SimpleGrid, Icon, Text, Stack, Flex, useColorModeValue } from '@chakra-ui/react';
+import { IoAnalyticsSharp, IoLogoBitcoin, IoSearchSharp } from 'react-icons/io5';
 
 const Feature = ({ title, text, icon }) => {
     return (
@@ -10,10 +9,11 @@ const Feature = ({ title, text, icon }) => {
                 h={16}
                 align={'center'}
                 justify={'center'}
-                color={'white'}
-                rounded={'full'}
-                bg={'gray.100'}
-                mb={1}>
+                color={useColorModeValue('yellow.300', 'yellow.600')}
+                bg={useColorModeValue('yellow.600', 'yellow.900')}
+                rounded={'3xl'}
+                mb={1}
+            >
                 {icon}
             </Flex>
             <Text fontWeight={600}>{title}</Text>
@@ -26,27 +26,44 @@ export default function ThreeColumns() {
     return (
         <Box
             p={4}
-            fontFamily={"'Rajdhani', sans-serif"}
-            fontSize={"lg"}
+            fontFamily={"'Etna', sans-serif"}
+            fontSize={"xl"}
             fontWeight={"regular"}
         >
             <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
                 <Feature
-                    icon={<Icon as={DiRust} w={10} h={10} color={"#000000"} />}
+                    icon={
+                        <Icon as={IoAnalyticsSharp}
+                            w={10}
+                            h={10}
+
+                        />
+                    }
                     title={'Lifetime Support'}
                     text={
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
                     }
                 />
                 <Feature
-                    icon={<Icon as={DiGo} w={10} h={10} color={"#000000"} />}
+                    icon={
+                        <Icon as={IoLogoBitcoin}
+                            w={10}
+                            h={10}
+                        />
+                    }
                     title={'Unlimited Donations'}
                     text={
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
                     }
                 />
                 <Feature
-                    icon={<Icon as={DiVim} w={10} h={10} color={"#000000"}/>}
+                    icon={
+                        <Icon as={IoSearchSharp}
+                            w={10}
+                            h={10}
+                            color={""}
+                        />
+                    }
                     title={'Instant Delivery'}
                     text={
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
