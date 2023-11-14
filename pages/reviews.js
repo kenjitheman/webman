477c3a26dfd133cf6117ba9a731ff79d2ca77fae
kenjitheman <kenjith3man@gmail.com>
2023-11-14 20:56:51 +0200

    feat: reviews page

--- pages/reviews.js
+++ pages/reviews.js
@@ -5,10 +5,13 @@ import Reviews from "../components/reviews.js";
 
 const AllReviews = () => {
     return (
-    <Layout title="Reviews | Alt Corporation">
+        <Layout
+            title="Reviews | Alt Corporation"
+            description="Alt Corporation reviews page. Here you can find out what people think about us."
+        >
             <Flex flexDirection={'column'}>
-                <Container maxW="8xl" pt={10}>
-                    <Section delay={0.1}>
+                <Container maxW="8xl" py={12}>
+                    <Section delay={0.2}>
                         <Reviews />
                     </Section>
                 </Container>
