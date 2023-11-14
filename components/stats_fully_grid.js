6be0cb56b0985d1b58bc9a8be0d03363641bafd4
kenjitheman <kenjith3man@gmail.com>
2023-11-14 21:02:34 +0200

    feat: fully grid stats init

---
+++ components/stats_fully_grid.js
@@ -0,0 +1,124 @@
+import {
+    GridItem,
+    Box,
+    Grid,
+    Flex,
+    Heading,
+    Text,
+    Icon,
+    useColorModeValue,
+} from '@chakra-ui/react';
+import { IoAnalyticsSharp, IoLogoBitcoin, IoSearchSharp } from 'react-icons/io5';
+import { React } from 'react';
+import { AnimateOnHover } from '../components/custom_animations';
+
+const StatFullyGrid = ({ text, icon }) => {
+    return (
+        <AnimateOnHover>
+            <Flex
+                flexDirection="row"
+                alignItems="center"
+                px={{ base: 4, md: 15 }}
+                my={5}
+                py={10}
+                shadow={'lg'}
+                border={'1px solid'}
+                borderColor={useColorModeValue('gray.300', 'gray.700')}
+                rounded={'3xl'}
+            >
+                <Flex
+                    w={12}
+                    h={12}
+                    p={12}
+                    align={'center'}
+                    justify={'space-evenly'}
+                >
+                    {icon}
+                </Flex>
+                <Text fontSize={'2xl'} fontWeight={'medium'}>
+                    {text}
+                </Text>
+            </Flex>
+        </AnimateOnHover>
+    );
+};
+
+export default function StatsFullyGrid() {
+    return (
+        <Box
+            fontFamily={"'Etna', sans-serif"}
+            rounded={'3xl'}
+            w={'100%'}
+            py={10}
+        >
+            <Heading
+                fontFamily={"'Etna', sans-serif"}
+                py={3}
+                textAlign={'left'}
+                fontSize={'4xl'}
+            >
+                A digital Product design agency</Heading>
+            <Text color={'gray.500'} fontSize={'2xl'}>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor invidunt ut labore
+            </Text>
+            <Flex
+                w={'100%'}
+                justifyContent={'space-between'}
+                flexDirection={{ base: 'column', md: 'row' }}
+                alignItems={'center'}
+            >
+                <Grid
+                    borderRadius={'10xl'}
+                    gap={4}
+                    w={'100%'}
+                    columns={1}
+                >
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 4 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoAnalyticsSharp} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Business Planning'}
+                        />
+                    </GridItem>
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 2 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoLogoBitcoin} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Financial Planning'}
+                        />
+                    </GridItem>
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 2 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoSearchSharp} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Market Analysis'}
+                        />
+                    </GridItem>
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 1 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoSearchSharp} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Market Analysis'}
+                        />
+                    </GridItem>
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 3 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoSearchSharp} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Market Analysis'}
+                        />
+                    </GridItem>
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 3 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoSearchSharp} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Market Analysis'}
+                        />
+                    </GridItem>
+                    <GridItem rowSpan={4} colSpan={{ base: 0, md: 1 }} >
+                        <StatFullyGrid
+                            icon={<Icon as={IoSearchSharp} color={useColorModeValue('yellow.500', 'yellow.700')} w={12} h={12} />}
+                            text={'Market Analysis'}
+                        />
+                    </GridItem>
+                </Grid>
+            </Flex>
+        </Box>
+    );
+};
+
