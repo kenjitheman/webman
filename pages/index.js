8098d83cd2a93662275e86fe6a7809c38a8f3eeb
kenjitheman <kenjith3man@gmail.com>
2023-11-14 21:14:29 +0200

    chore: delete components no more needed from index.js, add new changed components

--- pages/index.js
+++ pages/index.js
@@ -6,56 +6,58 @@ import {
 import Layout from "../components/layouts/article";
 import Section from "../components/section";
 import Hero from "../components/hero.js";
-import Statistics from "../components/statistics.js";
-import Superpowers from "../components/superpowers.js";
 import Community from "../components/community.js";
 import Features from "../components/features.js";
-import Feature_3 from "../components/feature_3.js";
-
+import WelcomeStats from "../components/welcome_stats.js";
+import StatsFullyGrid from "../components/stats_fully_grid.js";
+import StatsWithGrid from "../components/stats_with_grid.js";
 
 const Home = () => {
     return (
-        <Layout>
+        <Layout
+            title="Home | Alt Corporation"
+            description="Welcome to Alt Corporation home page."
+        >
             <Flex flexDirection={'column'}>
                 <Box>
                     <Container maxW="10xl">
-                        <Section delay={0.2}>
+                        <Section delay={0.5}>
                             <Hero />
                         </Section>
                     </Container>
                 </Box>
                 <Box>
                     <Container maxW="10xl">
-                        <Section delay={0.4}>
-                            <Statistics />
-                        </Section>
-                    </Container>
-                </Box>
-                <Box>
-                    <Container maxW="8xl">
-                        <Section delay={0.3}>
-                            <Community />
-                        </Section>
-                    </Container>
-                </Box>
-                <Box>
-                    <Container maxW="8xl">
-                        <Section delay={1}>
-                            <Superpowers />
-                        </Section>
-                    </Container>
-                </Box>
-                <Box>
-                    <Container maxW="8xl">
-                        <Section delay={0.5}>
-                            <Features />
+                        <Section delay={0.8}>
+                            <WelcomeStats />
                         </Section>
                     </Container>
                 </Box>
                 <Box>
                     <Container maxW="8xl">
                         <Section delay={0.9}>
-                            <Feature_3 />
+                            <StatsFullyGrid />
+                        </Section>
+                    </Container>
+                </Box>
+                <Box>
+                    <Container maxW="8xl">
+                        <Section delay={1}>
+                            <Features />
+                        </Section>
+                    </Container>
+                </Box>
+                <Box>
+                    <Container maxW="8xl">
+                        <Section delay={1.2}>
+                            <StatsWithGrid />
+                        </Section>
+                    </Container>
+                </Box>
+                <Box>
+                    <Container maxW="8xl">
+                        <Section delay={1.4}>
+                            <Community />
                         </Section>
                     </Container>
                 </Box>
@@ -65,4 +67,3 @@ const Home = () => {
 };
 
 export default Home;
-export { getServerSideProps } from "../components/chakra";
