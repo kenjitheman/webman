454a3e398336daeb9265c06bf414f367680faa2c
kenjitheman <kenjith3man@gmail.com>
2023-11-15 23:56:03 +0200

    feat: update logo (add rotating effect, and auto resizing)

--- components/logo.js
+++ components/logo.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Text, useColorModeValue } from "@chakra-ui/react";
 import Image from "next/image";
 import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
 const LogoBox = styled.span`
 font-weight: bold;
@@ -11,17 +12,23 @@ align-items: center;
 height: 55px;
 line-height: 20px;
 padding: 10px;
+cursor: pointer;
 `;
 
 const Logo = () => {
     return (
         <Link href="/" scroll={false}>
             <LogoBox>
-                <Image
-                    src="/images/logo.png"
-                    width={50}
-                    height={50}
-                />
+                <motion.div
+                    animate={{ rotate: 360 }}
+                    transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
+                >
+                    <Image
+                        src="/images/logo.png"
+                        width={50}
+                        height={50}
+                    />
+                </motion.div>
                 <Text
                     color={useColorModeValue("gray.800", "whiteAlpha.900")}
                     fontFamily='"Etna", sans-serif'
@@ -32,7 +39,7 @@ const Logo = () => {
                     Alt Corporation
                 </Text>
             </LogoBox>
-        </Link>
+        </Link >
     );
 };
 
