9e440adf77c32b23d5e103b3c3e0c9f43acae0f1
kenjitheman <kenjith3man@gmail.com>
2023-11-19 16:47:56 +0200

    chore: change colors to previous, change width

--- components/welcome_stats.js
+++ components/welcome_stats.js
@@ -14,52 +14,39 @@ function WelcomeStat(props) {
     const { title, stat, icon } = props;
     return (
         <AnimateOnHover>
-            <Stat>
-                <Box
-                    px={{ base: 2, md: 12 }}
-                    py={8}
-                    shadow={'xl'}
-                    border={'1px solid'}
-                    borderColor={useColorModeValue('gray.300', 'gray.900')}
-                    style={{
-                        backgroundImage: `url(/images/background.jpg)`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }}
-                    rounded={'3xl'}
-                >
-                    <Flex justifyContent={'space-evenly'}>
-                        <Box pl={{ base: 2, md: 4 }} >
-                            <StatNumber
-                                fontSize={{ base: '2xl', xl: '3xl' }}
-                                fontWeight={'bold'}
-                                color={useColorModeValue('gray.300', 'gray.300')}
-                            >
-                                {stat}
-                            </StatNumber>
-                            <StatLabel
-                                fontSize={{ base: 'xl', md: 'xl', xl: '2xl' }}
-                                fontWeight={'regular'}
-                                isTruncated
-                                color={useColorModeValue('gray.300', 'gray.300')}
-                            >
-                                {title}
-                            </StatLabel>
-                        </Box>
-                        <Box my={'auto'} color={useColorModeValue('gray.300', 'gray.200')} alignContent={'center'}>
-                            {icon}
-                        </Box>
-                    </Flex>
-                </Box>
+            <Stat
+                px={{ base: 2, md: 12 }}
+                py={8}
+                shadow={'xl'}
+                border={'1px solid'}
+                borderColor={useColorModeValue('gray.300', 'gray.700')}
+                css={{ backdropFilter: "blur(10px)" }}
+                rounded={'3xl'}
+                _hover={{
+                    cursor: 'pointer',
+                }}
+            >
+                <Flex justifyContent={'space-evenly'}>
+                    <Box pl={{ base: 2, md: 4 }} >
+                        <StatNumber fontSize={{ base: '2xl', xl: '3xl' }} fontWeight={'bold'}>
+                            {stat}
+                        </StatNumber>
+                        <StatLabel fontSize={{ base: 'xl', md: 'xl', xl: '2xl' }} fontWeight={'regular'} isTruncated>
+                            {title}
+                        </StatLabel>
+                    </Box>
+                    <Box my={'auto'} color={useColorModeValue('yellow.500', 'yellow.700')} alignContent={'center'}>
+                        {icon}
+                    </Box>
+                </Flex>
             </Stat>
-        </AnimateOnHover >
+        </AnimateOnHover>
     );
 }
 
 export default function WelcomeStats() {
     return (
-        <Box maxW="8xl" mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
+        <Box maxW="10xl" mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
             <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
                 <WelcomeStat title={'Users'} stat={'5,000+'} icon={<IoSearchSharp size={'4rem'} />} />
                 <WelcomeStat title={'All Time Trades'} stat={'1,000+'} icon={<IoLogoBitcoin size={'4rem'} />} />
