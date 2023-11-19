ee854500b7a9096b2e3fc80da9382cf2b309943d
kenjitheman <kenjith3man@gmail.com>
2023-11-19 16:46:02 +0200

    chore: remove motion effect

--- components/logo.js
+++ components/logo.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { Text, useColorModeValue } from "@chakra-ui/react";
 import Image from "next/image";
 import styled from "@emotion/styled";
-import { motion } from "framer-motion";
 
 const LogoBox = styled.span`
 font-weight: bold;
@@ -19,16 +18,11 @@ const Logo = () => {
     return (
         <Link href="/" scroll={false}>
             <LogoBox>
-                <motion.div
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
-                >
-                    <Image
-                        src="/images/logo.png"
-                        width={50}
-                        height={50}
-                    />
-                </motion.div>
+                <Image
+                    src="/images/logo.png"
+                    width={50}
+                    height={50}
+                />
                 <Text
                     color={useColorModeValue("gray.800", "whiteAlpha.900")}
                     fontFamily='"Etna", sans-serif'
