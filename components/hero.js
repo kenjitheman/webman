6c573cc3fe4c5ed35554671b3f1560abcc06f782
kenjitheman <kenjith3man@gmail.com>
2023-11-19 16:46:59 +0200

    chore: change text align from center to left

--- components/hero.js
+++ components/hero.js
@@ -2,8 +2,8 @@ import {
     Box,
     Heading,
     Container,
+    Flex,
     Text,
-    Stack,
     useColorModeValue,
 } from '@chakra-ui/react';
 import { GetStartedButton, LearnMoreButton } from '../components/buttons';
@@ -11,14 +11,16 @@ import { GetStartedButton, LearnMoreButton } from '../components/buttons';
 export default function Hero() {
     return (
         <Box
-            display={{ md: 'flex', justifyContent: 'center', alignItems: 'center' }}
+            display={'flex'}
+            justifyContent={'flex-start'}
+            alignItems={'flex-end'}
             w={'100%'}
             h={{ base: 'xl', sm: '2xl', md: '850px' }}
             mx={'auto'}
             mt={10}
             rounded={'3xl'}
             border={'0.1px solid'}
-            borderColor={useColorModeValue('', 'gray.900')}
+            borderColor={useColorModeValue('gray.300', 'gray.900')}
             style={{
                 backgroundImage: `url(/images/background.jpg)`,
                 backgroundSize: 'cover',
@@ -26,41 +28,44 @@ export default function Hero() {
                 backgroundRepeat: 'no-repeat',
             }}
         >
-            <Container maxW={'5xl'}>
-                <Stack
-                    as={Box}
-                    textAlign={'center'}
-                    spacing={{ base: 8, md: 14 }}
-                    py={{ base: 20, md: 36 }}
+            <Container
+                maxW={'10xl'}
+                fontFamily={"'Etna', sans-serif"}
+                display={'flex'}
+                justifyContent={'space-between'}
+                alignItems={'flex-start'}
+                wrap={'wrap'}
+            >
+                <Box
+                    py={5}
+                    px={{ base: 0, sm: 5 }}
                 >
                     <Heading
                         fontWeight={600}
-                        fontSize={{ base: '3xl', sm: '5xl', md: '7xl' }}
+                        fontSize={{ base: '3xl', sm: '5xl', md: '6xl' }}
                         color={'gray.50'}
                     >
-                        Make money from <br />
-                        your audience
-                </Heading>
-                <Text
-                    fontSize={{ base: 'md', lg: 'xl' }}
-                    color={'gray.300'}
-                >
-                    Monetize your content by charging your most loyal readers and reward them
-                    loyalty points. Give back to your loyal readers by granting them access to
-                    your pre-releases and sneak-peeks.
-                </Text>
-                <Stack
-                    direction={{ base: 'column', sm: 'row', md: 'row' }}
-                    spacing={{ base: 7, sm: 5, md: 7 }}
-                    align={'center'}
-                    alignSelf={'center'}
-                    position={'relative'}
-                >
-                    <GetStartedButton />
-                    <LearnMoreButton />
-                </Stack>
-            </Stack>
-        </Container>
+                        Welcome to the future of DeFi
+                    </Heading>
+                    <Text
+                        fontSize={{ base: 'md', lg: 'xl' }}
+                        color={'gray.300'}
+                    >
+                        NewLife™ is a decentralized finance (DeFi) platform that aims to provide a complete solution for the agricultural industry.
+                    </Text>
+                    <Flex
+                        justifyContent="flex-start"
+                        py={5}
+                        wrap={'wrap'}
+                    >
+                        <Box mb={{ base: 2, sm: 0 }}>
+                            <GetStartedButton />
+                        </Box>
+                        <Box px={4}></Box>
+                        <LearnMoreButton />
+                    </Flex>
+                </Box>
+            </Container >
         </Box >
     );
 }
