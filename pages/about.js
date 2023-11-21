1905cea13e2d70d18f12bdb00bafecd6b4ab10c3
kenjitheman <kenjith3man@gmail.com>
2023-11-21 22:22:26 +0200

    chore: delete useless content

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,6 @@ import { Container, Box, Heading, Flex } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
-import SimpleAccordion from '../components/accordion.js'
 
 const About = () => (
     <Layout
@@ -38,21 +37,9 @@ const About = () => (
                     Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
                 </Paragraph>
                 <br />
-                <Section delay={0.6}>
-                    <Flex
-                        flexDirection={{ base: 'column', md: 'row' }}
-                    >
-                        <Box w={{ base: '100%', md: '50%' }}>
-                            <SimpleAccordion />
-                        </Box>
-                        <Box w={{ base: '100%', md: '50%' }}>
-                            <SimpleAccordion />
-                        </Box>
-                    </Flex>
-                </Section>
             </Section>
         </Container>
-    </Layout>
+    </Layout >
 )
 
 export default About
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,35 +1,42 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Flex } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
-import CallToAction from '../components/call_to_action.js'
 
 const ContactUs = () => (
     <Layout
-        title="Contact Us | Alt Corporation"
-        description="Alt Corporation contact page. Here you can find all the information about ways to contact us."
+        title="About | Alt Corporation"
+        description="Alt Corporation is a organization that is focused on creating a better future for everyone."
     >
-        <Container maxW="7xl">
-            <Heading as="h1" fontSize={35} mt={12} mb={12} textAlign={"center"}>
-                Contact Us
+        <Container maxW="7xl" fontFamily={"'Etna', sans-serif"}>
+            <Heading
+                as="h1"
+                fontSize={'2xl'}
+                mt={12}
+                mb={12}
+                textAlign={"center"}
+            >
+                About Us
             </Heading>
-            <Section delay={0.1} mb={7} >
+            <Section
+                delay={0.1}
+                mb={7}
+            >
                 <Paragraph >
                     Welcome to our little project!
                 </Paragraph>
             </Section>
-            <Section delay={0.3} mb={12} >
+            <Section delay={0.3}>
+                <Paragraph>
+                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
+                </Paragraph>
+                <Paragraph>
+                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
+                </Paragraph>
                 <Paragraph>
                     Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
                 </Paragraph>
                 <br />
-                <Container maxW="8xl">
-                    <Section delay={0.7}>
-                        <Box>
-                            <CallToAction />
-                        </Box>
-                    </Section>
-                </Container>
             </Section>
         </Container>
     </Layout >
