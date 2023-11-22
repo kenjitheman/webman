f5f1716c64887d113a5aacc8ce26c83914a1f4f7
kenjitheman <kenjith3man@gmail.com>
2023-11-22 22:31:20 +0200

    chore: add padding, blur, change text, refactor args

--- components/hero.js
+++ components/hero.js
@@ -4,10 +4,12 @@ import {
     Container,
     Flex,
     Text,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import { GetStartedButton, LearnMoreButton } from '../components/buttons';
 
-export default function Hero() {
+const Hero = (props) => {
+    const { title, text } = props;
     return (
         <Box
             display={'flex'}
@@ -15,7 +17,7 @@ export default function Hero() {
             alignItems={'flex-end'}
             w={'100%'}
             h={{ base: 'xl', sm: '2xl', md: '850px' }}
-            bgImage={'url(/images/background.jpg)'}
+            bgImage={useColorModeValue('url(/images/background_white.jpg)', 'url(/images/background.jpg)')}
             bgSize={'cover'}
             bgPosition={'center'}
             bgRepeat={'no-repeat'}
@@ -30,28 +32,36 @@ export default function Hero() {
                 justifyContent={'space-between'}
                 alignItems={'flex-start'}
                 wrap={'wrap'}
+                p={5}
             >
                 <Box
                     py={5}
                     px={{ base: 0, sm: 5 }}
+                    rounded={'3xl'}
+                    backdropFilter={'blur(5px)'}
+                    border={'1px solid'}
+                    borderColor={useColorModeValue('gray.300', 'gray.700')}
                 >
                     <Heading
                         fontWeight={600}
                         fontSize={{ base: '3xl', sm: '5xl', md: '6xl' }}
-                        color={'gray.50'}
+                        color={useColorModeValue('gray.900', 'white')}
+                        p={3}
                     >
-                        Welcome to the future of DeFi
+                        {title}
                     </Heading>
                     <Text
                         fontSize={{ base: 'md', lg: 'xl' }}
-                        color={'gray.300'}
+                        color={useColorModeValue('gray.500', 'gray.300')}
+                        p={3}
                     >
-                        NewLife™ is a decentralized finance (DeFi) platform that aims to provide a complete solution for the agricultural industry.
+                        {text}
                     </Text>
                     <Flex
                         justifyContent="flex-start"
                         py={5}
                         wrap={'wrap'}
+                        p={3}
                     >
                         <Box mb={{ base: 2, sm: 0 }}>
                             <GetStartedButton />
@@ -64,3 +74,5 @@ export default function Hero() {
         </Box >
     );
 }
+
+export { Hero };
