55f2efe7fbb90988a1e8feefac963b4906ac5a2c
kenjitheman <kenjith3man@gmail.com>
2023-11-22 22:27:44 +0200

    refactor: change args implementation in component

--- components/features.js
+++ components/features.js
@@ -1,60 +1,40 @@
-import { Box, SimpleGrid, Icon, Text, Stack, Flex, useColorModeValue } from '@chakra-ui/react';
-import { IoAnalyticsSharp, IoLogoBitcoin, IoSearchSharp } from 'react-icons/io5';
+import { Text, Stack, Flex, useColorModeValue, Box } from '@chakra-ui/react';
 import { AnimateOnHover } from '../components/custom_animations';
 
-const Feature = ({ title, text, icon }) => {
+const Feature = (props) => {
+    const { title, text, icon } = props;
     return (
-        <Stack>
-            <AnimateOnHover>
-                <Flex
-                    w={16}
-                    h={16}
-                    align={'center'}
-                    justify={'center'}
-                    color={useColorModeValue('yellow.500', 'yellow.700')}
-                    border={'1px solid'}
-                    borderColor={useColorModeValue('gray.300', 'gray.700')}
-                    shadow={'lg'}
-                    rounded={'3xl'}
-                    mb={1}
-                    _hover={{
-                        cursor: 'pointer',
-                    }}
-                >
-                    {icon}
-                </Flex>
-                <Text fontWeight={"medium"}>{title}</Text>
-                <Text color={'gray.600'}>{text}</Text>
-            </AnimateOnHover>
-        </Stack>
+        <AnimateOnHover>
+            <Box
+                w="100%"
+                border={'1px solid'}
+                borderColor={useColorModeValue('gray.300', 'gray.700')}
+                shadow={'lg'}
+                _hover={{
+                    cursor: 'pointer',
+                }}
+                rounded={'3xl'}
+                p={4}
+            >
+                <Stack>
+                    <Flex
+                        w={16}
+                        h={16}
+                        align={'center'}
+                        justify={'center'}
+                        color={useColorModeValue('yellow.500', 'yellow.700')}
+                        border={'1px solid'}
+                        borderColor={useColorModeValue('gray.300', 'gray.700')}
+                        rounded={'3xl'}
+                    >
+                        {icon}
+                    </Flex>
+                    <Text fontWeight={"medium"}>{title}</Text>
+                    <Text color={'gray.600'}>{text}</Text>
+                </Stack >
+            </Box >
+        </AnimateOnHover>
     );
 };
 
-export default function ThreeColumns() {
-    return (
-        <Box
-            p={4}
-            fontFamily={"'Etna', sans-serif"}
-            fontSize={"2xl"}
-            fontWeight={"regular"}
-        >
-            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-                <Feature
-                    icon={<Icon as={IoAnalyticsSharp} w={10} h={10} />}
-                    title={'Lifetime Support'}
-                    text={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'}
-                />
-                <Feature
-                    icon={<Icon as={IoLogoBitcoin} w={10} h={10} />}
-                    title={'Unlimited Donations'}
-                    text={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporinvidunt ut labore...'}
-                />
-                <Feature
-                    icon={<Icon as={IoSearchSharp} w={10} h={10} color={""} />}
-                    title={'Instant Delivery'}
-                    text={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'}
-                />
-            </SimpleGrid>
-        </Box>
-    );
-}
+export { Feature };
