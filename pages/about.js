58363caafc2c779e2b1151714011b33a935ac874
kenjitheman <kenjith3man@gmail.com>
2023-11-22 22:15:28 +0200

    chore: delete useless content (change it on just text instead)

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ const About = () => (
         title="About | Alt Corporation"
         description="Alt Corporation is a organization that is focused on creating a better future for everyone."
     >
-        <Container maxW="7xl" fontFamily={"'Etna', sans-serif"}>
+        <Container maxW="7xl">
             <Heading
                 as="h1"
                 fontSize={'2xl'}
@@ -28,15 +28,108 @@ const About = () => (
             </Section>
             <Section delay={0.3}>
                 <Paragraph>
-                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
-                </Paragraph>
-                <Paragraph>
-                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
-                </Paragraph>
-                <Paragraph>
-                    Thanks for chenking us out. We are Next Space Lab! A small startup and hackaton team from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
                 </Paragraph>
                 <br />
+                <Paragraph>
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                    However, we are always looking for new team members.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
+                </Paragraph>
+                <br />
+                <Paragraph>
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                    However, we are always looking for new team members.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                </Paragraph>
+                <br />
+                <Paragraph>
+                    However, we are always looking for new team members.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                    However, we are always looking for new team members.
+                </Paragraph>
             </Section>
         </Container>
     </Layout >
--- pages/our-team.js
+++ pages/our-team.js
@@ -8,7 +8,7 @@ const OurTeam = () => (
         title="Our Team | Alt Corporation"
         description="Alt Corporation team page. Here you can find all the information about our team."
     >
-        <Container>
+        <Container maxW={'7xl'}>
             <Heading as="h1" fontSize={35} mt={12} mb={12} textAlign={"center"}>
                 Our Team
             </Heading>
@@ -19,11 +19,107 @@ const OurTeam = () => (
             </Section>
             <Section delay={0.2} mb={12} >
                 <Paragraph>
-                    Thanks for cheking our team page, we are the biggest in Ukraine and we are proud of it! We are Alt Corporation and we are the best! We hope you will enjoy with us!
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
                 </Paragraph>
                 <br />
                 <Paragraph>
-                    We are a small team of developers and designers from Ukraine. We are spread all around the globe but still gathering every so often to do something cool.
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                    However, we are always looking for new team members.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
+                </Paragraph>
+                <br />
+                <Paragraph>
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                    However, we are always looking for new team members.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                </Paragraph>
+                <br />
+                <Paragraph>
+                    However, we are always looking for new team members.
+                    Thanks for chenking us out. We are Next Space Lab!
+                    A small startup and hackaton team from Ukraine.
+                    We are spread all around the globe but still gathering every so often to do something cool.
+                    We are focused on creating a better future for everyone.
+                    Get in touch with us if you want to help us with our mission.
+                    Visit our central hub to learn more about us and our projects.
+                    We are always open to new ideas and suggestions.
+                    And we are always looking for new team members.
+                    Maybe you are the one we are looking for?
+                    Or maybe you just want to say hi?
+                    Reach out to us on social media or send us an email.
+                    We will be happy to hear from you!
+                    Questions? Suggestions? Ideas? We are always open to new ideas and suggestions.
+                    Be sure to check out our social media and our central hub.
+                    There you can find more information about us and our projects.
+                    Need to contact us? Send us an email or reach out to us on social media.
+                    We will be happy to hear from you!
+                    You can also join our Discord server.
+                    Reach out to us on social media or send us an email.
+                    Please note that we are not a company.
+                    We are a small team of developers and designers.
+                    We are not a company.
+                    However, we are always looking for new team members.
                 </Paragraph>
             </Section>
         </Container>
