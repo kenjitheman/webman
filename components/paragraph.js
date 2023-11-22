8ce5042096e91a40ea331936afa2d8531f697afc
kenjitheman <kenjith3man@gmail.com>
2023-11-22 22:29:07 +0200

    chore: add font family to paragraph component

--- components/paragraph.js
+++ components/paragraph.js
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
 
 const Paragraph = styled.p`
-  text-align: justify;
-	font-size: 1.2rem;
-  hyphens: auto;
+font-family: 'Etna', sans-serif;
+text-align: justify;
+font-size: 1.4rem;
+hyphens: auto;
 `;
 
 export default Paragraph;
