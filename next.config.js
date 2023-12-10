ed9336c4e8be29b4b582b40c24a2720668fcca5b
kenjitheman <kenjith3man@gmail.com>
2023-12-10 03:18:58 +0200

    feat: add next-i18next config, next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-    i18n: {
-        locales: ['en-US', 'uk-UK', 'ru-RU'],
-        defaultLocale: 'en-US',
-    },
-});
+const { i18n } = require('./next-i18next.config');
+module.exports = {
+    i18n,
+};
