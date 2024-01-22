54245cd92fc34c87ec0defc9ac85f34d437df9e9
btwkenji <btwdesu@gmail.com>
2024-01-22 07:36:50 +0200

    fix: cannot use import statement outside a module

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true"
 });
 
-import { i18n } from "./next-i18next.config";
-export default {
+const { i18n } = require("./next-i18next.config");
+module.exports = {
     i18n
 };
