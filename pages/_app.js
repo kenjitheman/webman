cc96884eb9eff004393e3d00101c9611e764872b
btwkenji <btwdesu@gmail.com>
2024-01-26 06:05:17 +0200

    chore: i don't remember what i've changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { appWithTranslation } from "next-i18next";
 import Chakra from "../components/chakra";
 import Fonts from "../components/fonts";
 import Layout from "../components/layouts/main";
 import { AnimatePresence } from "framer-motion";
-import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../lib/theme";
+import { appWithTranslation } from "next-i18next";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
