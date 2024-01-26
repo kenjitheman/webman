bf8ab5b6e54528a9d9e99221245c94b0a8597f05
btwkenji <btwdesu@gmail.com>
2024-01-26 05:56:13 +0200

    feat: add new, refactored, fixed navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,21 +3,24 @@ import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import Logo from "../components/logo.js";
 import { useRouter } from "next/router";
 import ThemeToggleButton from "../components/theme-toggle-button.js";
-import { FaDiscord, FaTwitter, FaGithub } from "react-icons/fa";
+import { FaDiscord, FaTelegram, FaInstagram } from "react-icons/fa";
 import LanguageSwitcher from "./language_switcher.js";
-import dynamic from "next/dynamic";
+import Scroll from "react-scroll";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { IoMail } from "react-icons/io5";
+import Link from "next/link.js";
 
-const ScrollLink = dynamic(() => import("react-scroll").then((module) => module.Link), {
-    ssr: false,
-    loading: () => null
-});
+const ScrollLink = Scroll.Link;
+
 
 const Links = {
-    "About": "#about-us",
-    "Reviews": "https://t.me/alt_portfolio",
-    "Services": "#services"
+    home: "#home",
+    about_us: "#about-us",
+    services: "#services",
+    contacts: "#contacts",
+    reviews: "https://t.me/alt_corp"
 };
-
 
 const NavLink = ({ children, href, currentPath }) => {
     const isActive = href === currentPath;
@@ -30,13 +33,12 @@ const NavLink = ({ children, href, currentPath }) => {
                 as="a"
                 px={5}
                 py={2}
-                rounded="2xl"
                 textDecoration="none"
+                color={useColorModeValue("gray.900", "white")}
                 _hover={{
                     textDecoration: "none",
-                    bg: useColorModeValue("white_yellow", "dark_yellow")
+                    color: useColorModeValue("white_yellow", "dark_yellow")
                 }}
-                bg={isActive ? useColorModeValue("white_yellow", "dark_yellow") : useColorModeValue("gray.200", "gray.900")}
                 href={href}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -55,11 +57,12 @@ const NavLink = ({ children, href, currentPath }) => {
                 smooth={true}
                 offset={-70}
                 duration={500}
+                color={useColorModeValue("gray.900", "white")}
                 style={{
                     textDecoration: "none",
-                    color: useColorModeValue("gray.900", "gray.300"),
                     _hover: {
-                        cursor: "pointer"
+                        cursor: "pointer",
+                        color: useColorModeValue("white_yellow", "dark_yellow")
                     }
                 }}
             >
@@ -67,12 +70,12 @@ const NavLink = ({ children, href, currentPath }) => {
                     px={5}
                     py={2}
                     rounded="2xl"
+                    color={useColorModeValue("gray.900", "white")}
                     _hover={{
                         textDecoration: "none",
-                        bg: useColorModeValue("white_yellow", "dark_yellow"),
-                        cursor: "pointer"
+                        cursor: "pointer",
+                        color: useColorModeValue("white_yellow", "dark_yellow")
                     }}
-                    bg={isActive ? useColorModeValue("white_yellow", "dark_yellow") : useColorModeValue("gray.200", "gray.900")}
                 >
                     {children}
                 </Box>
@@ -103,11 +106,12 @@ const NavLink = ({ children, href, currentPath }) => {
 
 };
 
-export default function Navbar() {
+const Navbar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const router = useRouter();
     const currentPath = router.asPath;
     const linksArray = Object.entries(Links);
+    const { t } = useTranslation("common");
 
     return (
         <>
@@ -119,13 +123,11 @@ export default function Navbar() {
                 px={4} py={2}
                 zIndex={2}
                 onScroll={onClose}
-                borderBottom="0.1px solid"
-                borderColor={useColorModeValue("gray.300", "gray.900")}
             >
                 <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
                     <HStack w={"100%"} spacing={2} alignItems={"center"} justifyContent={"flex-start"}>
                         <Box>
-                            <Logo />
+                            <Logo href={Links.home} />
                         </Box>
                         <HStack
                             as={"nav"}
@@ -137,10 +139,9 @@ export default function Navbar() {
                         >
                             {linksArray.map(([label, link]) => (
                                 <NavLink key={label} href={link} currentPath={currentPath}>
-                                    {label}
+                                    {t(`navbar.${label}`)}
                                 </NavLink>
                             ))}
-
                         </HStack>
                     </HStack>
                     <HStack
@@ -150,15 +151,30 @@ export default function Navbar() {
                     >
                         <LanguageSwitcher id={"language-switcher-nav"} />
                         <ThemeToggleButton />
-                        <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Discord">
+                        <Link
+                            href="https://discord.gg/XY2rMVUFhR"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Discord"
+                        >
                             <FaDiscord size={30} />
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                            <FaTwitter size={30} />
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Github">
-                            <FaGithub size={30} />
-                        </a>
+                        </Link>
+                        <Link
+                            href="https://t.me/alt_portfolio"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Telegram"
+                        >
+                            <FaTelegram size={30} />
+                        </Link>
+                        <Link
+                            href="mailto:"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Email"
+                        >
+                            <IoMail size={35} />
+                        </Link>
                     </HStack>
                     <IconButton
                         size={"lg"}
@@ -171,23 +187,51 @@ export default function Navbar() {
 
                 {isOpen ? (
                     <Box p={6} display={{ lg: "none" }}>
-                        <Stack as={"nav"} spacing={7}>
+                        <Stack
+                            as={"nav"}
+                            spacing={7}
+                            fontFamily={"'Etna', sans-serif"}
+                            fontSize={"lg"}
+                            fontWeight={"medium"}
+                            alignItems={"center"}
+                        >
                             {linksArray.map(([label, link]) => (
                                 <NavLink key={label} href={link} currentPath={currentPath}>
-                                    {label}
+                                    {t(`navbar.${label}`)}
                                 </NavLink>
                             ))}
-                            <Box justifyContent={"space-between"} display={"flex"} alignItems={"center"} >
+                            <Box justifyContent={"space-between"} display={"flex"} w={"100%"}>
                                 <Stack direction={"row"} spacing={3}>
-                                    <a href="#" target="_blank" rel="noopener noreferrer">
+                                    <Link
+                                        href="https://discord.gg/XY2rMVUFhR"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
                                         <FaDiscord size={30} />
-                                    </a>
-                                    <a href="#" target="_blank" rel="noopener noreferrer">
-                                        <FaTwitter size={30} />
-                                    </a>
-                                    <a href="#" target="_blank" rel="noopener noreferrer">
-                                        <FaGithub size={30} />
-                                    </a>
+                                    </Link>
+                                    <Link
+                                        href="https://t.me/alt_portfolio"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <FaTelegram size={30} />
+                                    </Link>
+                                    <Link
+                                        href="#"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        aria-label="Instagram"
+                                    >
+                                        <FaInstagram size={35} />
+                                    </Link>
+                                    <Link
+                                        href="mailto:"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        aria-label="Email"
+                                    >
+                                        <IoMail size={35} />
+                                    </Link>
                                 </Stack>
                                 <ThemeToggleButton />
                             </Box>
@@ -198,4 +242,14 @@ export default function Navbar() {
             </Box >
         </>
     );
+};
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"]))
+        }
+    };
 }
+
+export default Navbar;
