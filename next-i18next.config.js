8b906bddca995ec01e577e52d4fe78f13722b96b
btwkenji <btwdesu@gmail.com>
2024-01-31 10:47:23 +0200

    chore: update next-i18next.config.js (returnObjects: true)

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,8 @@ module.exports = {
     debug: process.env.NODE_ENV === "development",
     i18n: {
         defaultLocale: "en",
-        locales: ["en", "uk", "ru"]
+        locales: ["en", "uk", "ru"],
+        returnObjects: true
     },
 
     localePath:
