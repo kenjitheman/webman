77e001094297f3b2c17cfe8a6597d61350be614d
kenjitheman <btwdesu@gmail.com>
2024-02-21 00:56:23 +0200

    feat: implement autoslider

---
+++ components/carousel.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Slider from "react-slick";
+import { Box } from "@chakra-ui/react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function Carousel({ items }) {
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 2,
+        speed: 6000,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        cssEase: "linear",
+        centerMode: true,
+        pauseOnHover: false,
+        arrows: false,
+        initialSlide: 0
+    };
+
+    return (
+        <Box className="slider-container" w={"100%"} m={"auto"}>
+            <Slider {...settings}>
+                {items.map((item, index) => (
+                    <div key={index}>{item}</div>
+                ))}
+            </Slider>
+        </Box >
+    );
+}
+
+export default Carousel;
