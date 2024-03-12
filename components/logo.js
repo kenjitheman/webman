ac715e1482ed84d2f1e9dc97bbc3ffd9ae89effd
kenjitheman <im@kenjitheman.dev>
2024-03-13 00:50:40 +0200

    feat(components/logo): clickable logo to swipe up

--- components/logo.js
+++ components/logo.js
@@ -1,10 +1,22 @@
 import Link from "next/link";
 import { Box } from "@chakra-ui/react";
 import Image from "next/image";
+import { animateScroll } from "react-scroll";
+
+const scrollToTop = () => {
+    animateScroll.scrollToTop({ duration: 800 });
+};
 
 const Logo = () => {
     return (
-        <Link href="/" passHref scroll={false} aria-label="Home" label="Home">
+        <Link
+            href="/"
+            passHref
+            scroll={true}
+            aria-label="Home"
+            label="Home"
+            onClick={scrollToTop}
+        >
             <Box p={2}>
                 <Image
                     src="/images/logo_2d.png"
