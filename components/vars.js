9ecd4c68b510215798cb9b4b3465fad44948a900
kenjitheman <im@kenjitheman.dev>
2024-03-13 00:50:07 +0200

    feat(components): create vars.js

---
+++ components/vars.js
@@ -0,0 +1,19 @@
+const links = {
+    contacts: {
+        owner: "https://t.me/bybitsignals_01",
+        content: "https://t.me/alt_corporation",
+        traffic: "https://t.me/altcorp_traffic",
+        email: "info@altcorp.org"
+    },
+    socials: {
+        telegram: {
+            alt_corporation: "https://t.me/alt_portfolio",
+            owner: "https://t.me/+Y1wM__EfbHtjZGU0"
+        },
+        instagram: "https://www.instagram.com/temaaa_san/",
+        discord: "https://discord.gg/XY2rMVUFhR"
+    },
+    reviews: "https://t.me/+DATWMYlQTgA5Njc0"
+};
+
+export default links;
