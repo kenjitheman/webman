6a05e0273099d06b464fb4a1d949ada7620caf10
kenjitheman <im@kenjitheman.dev>
2024-03-13 16:53:44 +0200

    fix: lang switcher display lang

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     debug: process.env.NODE_ENV === "development",
     i18n: {
+        defaultLocale: "en",
         locales: ["en", "uk", "ru"],
         returnObjects: true
     },
